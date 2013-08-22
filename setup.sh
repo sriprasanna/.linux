b0821234cebd29199a3c44e33aefdb7bedb453f2
Sri Prasanna <k.sriprasanna@gmail.com>
2013-08-23 02:09:05 +0300

    changes

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 sudo pacman -S xorg-server xorg-xinit xorg-server-utils xf86-input-synaptics nvidia acpid
 sudo systemctl enable acpid
 sudo nvidia-xconfig
-sudo cp ~/.etc/X11/xorg.conf /etc/X11/xorg.conf
-sudo cp ~/.etc/X11/xorg.conf.d /etc/X11/xorg.conf.d
+sudo ln ~/.etc/X11/xorg.conf /etc/X11/xorg.conf
+sudo ln ~/.etc/X11/xorg.conf.d /etc/X11/xorg.conf.d
 yaourt -S xmonad xmonad-contrib cabal-install slim terminator xmobar dmenu-xft trayer scrot xscreensaver feh parcellite pidgin network-manager-applet xfce4-notifyd gnome-keyring git nautilus ranger fcitx-im
 cabal update && cabal install yeganesh
 sudo systemctl enable NetworkManager
